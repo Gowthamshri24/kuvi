69f33d055ce5e8bf56c3bec468963198fe9bff85
GowthamM <cityhuntcc@gmail.com>
2016-11-23 20:20:19 +0530

    Connectivity Service

--- src/main/java/com/alphanodus/kuvi/AccessKuvi.java
+++ src/main/java/com/alphanodus/kuvi/AccessKuvi.java
@@ -7,23 +7,29 @@ import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.JSONObject;
 
-public class AccessKuvi{
+public class AccessKuvi {
 
     private Context kuviContext;
     private static MqttClient mqttClient;
     private static IMqttToken token;
-    private static final String clientId = "demo_client";
-    private static final String BROKER_URL = "tcp://35.163.147.24:1883";
+    private static final String clientId = "embedded";
+    private static final String BROKER_URL = "tcp://35.164.64.228:1883";
     private final MqttConnectOptions options = new MqttConnectOptions();
-    private static final String username = "demo";
-    private static final String password = "emqtt";
+    public static final String DEVICE_LIST = "AN/Org/1/DevID/deviceList";
+    private static final String username = "embedded";
+    private static final String password = "password";
+    public final static String URL = "AN/org/1/dev/";
 
-    public AccessKuvi(Context context){
+
+    public AccessKuvi(Context context) {
         kuviContext = context;
         try {
-            mqttClient = new MqttClient(BROKER_URL,clientId,new MemoryPersistence());
+            mqttClient = new MqttClient(BROKER_URL, clientId, new MemoryPersistence());
             mqttClient.setCallback(new PushCallback(kuviContext));
             options.setCleanSession(true);
             options.setKeepAliveInterval(120);
@@ -34,7 +40,7 @@ public class AccessKuvi{
         }
     }
 
-    public IMqttToken connect(String UID,String KEY){
+    public IMqttToken connect(String UID, String KEY) {
 
         options.setCleanSession(true);
         options.setKeepAliveInterval(120);
@@ -45,7 +51,7 @@ public class AccessKuvi{
         if (!mqttClient.isConnected()) {
             try {
                 token = mqttClient.connectWithResult(options);
-                Log.d("connected","connected to" + BROKER_URL);
+                Log.d("connected", "connected to" + BROKER_URL);
             } catch (MqttException e) {
                 e.printStackTrace();
             }
@@ -54,7 +60,30 @@ public class AccessKuvi{
         return token;
     }
 
-    public void subscribe(String topic){
+
+
+    public void changeDeviceValue(String deviceType, int deviceId, String valueType, String msg) {
+        try {
+            if (mqttClient.isConnected()) {
+                String id = URL+deviceType+"/"+valueType;
+                mqttClient.publish(URL+deviceType+"/"+deviceId+"/"+valueType,msg.getBytes(), 2, false);
+                Log.d(Utils.LOG_TAG, "CHANGE DEVICE VALUE" + msg + "Published");
+            } else {
+                if (mqttClient.connectWithResult(options).isComplete()) {
+                    String id = URL+deviceType+"/"+valueType;
+                    mqttClient.publish(URL+deviceType+"/"+deviceId+"/"+valueType, msg.getBytes(), 2, false);
+
+                }
+            }
+
+        } catch (MqttPersistenceException e) {
+            e.printStackTrace();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void subscribe(String topic) {
 
         try {
             if (mqttClient.connectWithResult(options).isComplete()) {
@@ -64,14 +93,81 @@ public class AccessKuvi{
             e.printStackTrace();
         }
     }
+
     public static void publish(String topic, String msg) {
         try {
 
             mqttClient.publish(topic, msg.getBytes(), 2, false);
-            Log.d(Utils.LOG_TAG, "USER LOCATION" + msg + "Published to " + topic);
+            Log.d(Utils.LOG_TAG, "PUBLISHING STATUS" + msg + "Published to " + topic);
         } catch (MqttException e) {
             e.printStackTrace();
         }
     }
 
+    public void modifySetTemperature(String device, JSONObject val){
+        try {
+            if (mqttClient.isConnected()) {
+                String msg= String.valueOf(val);
+                mqttClient.publish(URL + device + "/" + "sptemp", msg.getBytes(), 2, false);
+                String url = URL+device+"/"+"sptemp";
+                int i =1;
+                Log.d(Utils.LOG_TAG, "CHANGE DEVICE VALUE" + val + "Published");
+            } else {
+                if (mqttClient.connectWithResult(options).isComplete()) {
+                    String msg= String.valueOf(val);
+                    mqttClient.publish(URL + device + "/" + "sptemp" , msg.getBytes(), 2, false);
+                }
+            }
+
+        } catch (MqttPersistenceException e) {
+            e.printStackTrace();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public void subscribeToDevice(String deviceId) {
+        try {
+            if(deviceId.charAt(0)=='T') {
+                if (mqttClient.isConnected()) {
+
+                        mqttClient.subscribe(URL + deviceId + "/" + "ambtemp");
+                    mqttClient.subscribe(URL + deviceId + "/" + "sptemp");
+                      mqttClient.subscribe(URL + deviceId + "/" + "spmode");
+                     mqttClient.subscribe(URL + deviceId + "/" + "hvacmode");
+
+                } else {
+                    if (mqttClient.connectWithResult(options).isComplete()) {
+                        //mqttClient.subscribe(URL + deviceId + "/" + "ambtemp");
+                        String temp = URL+deviceId+"/"+"sptemp";
+                        mqttClient.subscribe(URL + deviceId + "/" + "sptemp");
+                         mqttClient.subscribe(URL + deviceId + "/" + "spmode");
+                         mqttClient.subscribe(URL + deviceId + "/" + "hvacmode");
+                    }
+                }
+            }
+            else if(deviceId.charAt(0)=='A') {
+                if (mqttClient.isConnected()) {
+                    mqttClient.subscribe(URL + deviceId + "/" + "ambtemp");
+                     mqttClient.subscribe(URL + deviceId + "/" + "ambhum");
+                    mqttClient.subscribe(URL + deviceId + "/" + "motion");
+                } else {
+                    if (mqttClient.connectWithResult(options).isComplete()) {
+                        mqttClient.subscribe(URL + deviceId + "/" + "ambtemp");
+                          mqttClient.subscribe(URL + deviceId + "/" + "ambhum");
+                         mqttClient.subscribe(URL + deviceId + "/" + "motion");
+                    }
+
+                }
+            }
+        } catch (MqttSecurityException e) {
+            e.printStackTrace();
+        } catch (MqttException e) {
+            e.printStackTrace();
+
+        }
+    }
 }
